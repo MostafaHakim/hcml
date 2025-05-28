a0aa10e5786f7a4d529bbd58ac68059ee4f8421f
Md Mostafa <mdmostafahakim@gmail.com>
2025-05-28 18:41:44 +0600

    price get test

--- server/app.js
+++ server/app.js
@@ -8,13 +8,13 @@ const app = express();
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
 
-const partyRoute = require("./router/party.route");
-const dyesRoute = require("./router/dyes.route");
-const colorsRoute = require("./router/color.route");
-const vendorsRoute = require("./router/vendor.route");
-const demandRoute = require("./router/demand.route");
-const colorPriceRoute = require("./router/colorPrice.route");
-const griegeIn = require("./router/griegein.route");
+// const partyRoute = require("./router/party.route");
+// const dyesRoute = require("./router/dyes.route");
+// const colorsRoute = require("./router/color.route");
+// const vendorsRoute = require("./router/vendor.route");
+// const demandRoute = require("./router/demand.route");
+// const colorPriceRoute = require("./router/colorPrice.route");
+// const griegeIn = require("./router/griegein.route");
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -37,13 +37,13 @@ app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.use("/party", partyRoute);
-app.use("/proxy", dyesRoute);
-app.use("/color", colorsRoute);
-app.use("/vendor", vendorsRoute);
-app.use("/demand", demandRoute);
-app.use("/colorprice", colorPriceRoute);
-app.use("/griegein", griegeIn);
+// app.use("/party", partyRoute);
+// app.use("/proxy", dyesRoute);
+// app.use("/color", colorsRoute);
+// app.use("/vendor", vendorsRoute);
+// app.use("/demand", demandRoute);
+// app.use("/colorprice", colorPriceRoute);
+// app.use("/griegein", griegeIn);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at PORT ${PORT}`);
