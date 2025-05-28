9ae5833ebf31dad929b52d8b0bbf14756eedda8c
Md Mostafa <mdmostafahakim@gmail.com>
2025-05-28 19:18:04 +0600

    price get test

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
-const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
-const fetch = require("node-fetch");
+const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
-// const partyRoute = require("./router/party.route");
-// const dyesRoute = require("./router/dyes.route");
-// const colorsRoute = require("./router/color.route");
-// const vendorsRoute = require("./router/vendor.route");
-// const demandRoute = require("./router/demand.route");
-// const colorPriceRoute = require("./router/colorPrice.route");
-// const griegeIn = require("./router/griegein.route");
+const partyRoute = require("./router/party.route");
+const dyesRoute = require("./router/dyes.route");
+const colorsRoute = require("./router/color.route");
+const vendorsRoute = require("./router/vendor.route");
+const demandRoute = require("./router/demand.route");
+const colorPriceRoute = require("./router/colorPrice.route");
+const griegeIn = require("./router/griegein.route");
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
@@ -33,18 +31,25 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
+app.use("/party", partyRoute);
+app.use("/proxy", dyesRoute);
+app.use("/color", colorsRoute);
+app.use("/vendor", vendorsRoute);
+app.use("/demand", demandRoute);
+app.use("/colorprice", colorPriceRoute);
+app.use("/griegein", griegeIn);
 
-// app.use("/party", partyRoute);
-// app.use("/proxy", dyesRoute);
-// app.use("/color", colorsRoute);
-// app.use("/vendor", vendorsRoute);
-// app.use("/demand", demandRoute);
-// app.use("/colorprice", colorPriceRoute);
-// app.use("/griegein", griegeIn);
+// =====================Unknown URL =========================
+app.use((req, res, next) => {
+  res.status(404).json({ massage: "Page Not Found" });
+});
+// =====================Unknown Server Error =========================
+app.use((err, req, res, next) => {
+  res.status(500).json(err.massage);
+});
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running at PORT ${PORT}`);
+  console.log(`server is start at the port of ${PORT}`);
 });
+
+module.exports = app;
