6080ad210d7793fc19d4a212d1938c08e77b8505
Md Mostafa <mdmostafahakim@gmail.com>
2025-05-29 19:16:02 +0600

    price get test

--- server/router/party.route.js
+++ server/router/party.route.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createNewParty,
-  getParties,
-} = require("../controller/party.controller");
 
-// Get All Parties
-router.get("/", getParties);
+const GAS_BASE_URL =
+  "https://script.google.com/macros/s/AKfycbynqHtTEoBVXJMH0TGr62VCUGtW1aTRm1Z3UslIRBqZJ_I_yrcTGeUqXh0Fg4cyIi0Q/exec";
 
-// Create a New Party
-router.post("/", createNewParty);
+router.get("/", async (req, res) => {
+  try {
+    const response = await fetch(`${GAS_BASE_URL}?action=party`);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching colors:", error);
+    res.status(500).json({ error: "Failed to get color list" });
+  }
+});
 
 module.exports = router;
