2b495e282bdd184145bb5bf048d00a7721808c9e
MostafaHakim <89860668+MostafaHakim@users.noreply.github.com>
2025-05-29 22:00:52 +0600

    Update GriegeinForm.jsx

--- client/src/components/griege/GriegeinForm.jsx
+++ client/src/components/griege/GriegeinForm.jsx
@@ -143,7 +143,7 @@ export default function LotEntryForm() {
         className="border p-2 w-full"
       />
       <datalist id="partyNameList">
-        {partyList.map((item) => {
+        {partyList.map((item,i) => {
           return <option key={i} velue={item} />;
         })}
       </datalist>
