088fb1e027a24210a77a0048c789ba7ea7e84f28
Md Mostafa <mdmostafahakim@gmail.com>
2025-06-03 12:41:56 +0600

    stock

--- client/src/components/griege/Topbar.jsx
+++ client/src/components/griege/Topbar.jsx
@@ -4,12 +4,12 @@ function Topbar({ setTabOption }) {
   const tabOption = [...setTabOption];
   return (
     <div className="w-full flex flex-row items-start justify-center py-2 px-4">
-      <ul className="flex flex-row uppercase  bg-black bg-opacity-30  rounded-full overflow-hidden">
+      <ul className="flex flex-row uppercase  bg-black bg-opacity-30  rounded-full px-4 overflow-hidden">
         {tabOption.map((item, i) => {
           return (
             <NavLink
               to={item}
-              className="px-8 py-2 hover:bg-white hover:bg-opacity-10 transition duration-300"
+              className="px-1 md:px-8 py-2 hover:bg-white hover:bg-opacity-10 transition duration-300 text-xs md:text-md"
             >
               {item}
             </NavLink>
