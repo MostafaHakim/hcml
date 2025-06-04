1f2b1998513618a75b999270dbe417a4a19d5d4d
Md Mostafa <mdmostafahakim@gmail.com>
2025-06-04 10:06:01 +0600

    new com

--- client/src/components/store/Dyes.jsx
+++ client/src/components/store/Dyes.jsx
@@ -8,6 +8,8 @@ function Dyes() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const location = useLocation();
+  const showLocation = location.pathname == "/store/dyes";
   useEffect(() => {
     fetch(`https://hcml-ry8s.vercel.app/stock`)
       .then((res) => res.json())
@@ -25,33 +27,34 @@ function Dyes() {
   return (
     <div className="w-full flex flex-col items-center justify-center px-8 pb-8">
       <Topbar setTabOption={griegeOption} />
+      {showLocation ? (
+        <main className={` w-full`}>
+          {loading && <p className="text-gray-500">Loading...</p>}
+          {error && <p className="text-red-500">{error}</p>}
 
-      <main className={` w-full`}>
-        {loading && <p className="text-gray-500">Loading...</p>}
-        {error && <p className="text-red-500">{error}</p>}
-
-        <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 w-full text-sm">
-          {colorStock.map((color) => (
-            <div
-              key={color["PRODUCT NAME"]}
-              className="p-2 bg-white border border-gray-200 text-black flex flex-col items-start rounded-md shadow hover:bg-gray-50 transition"
-            >
-              <label className="font-semibold">{color["PRODUCT NAME"]}</label>
-              <p className="text-green-600">
-                Current Stock: <span>{color["PRESENT STOCK"]}</span>
-              </p>
-              <p className="text-yellow-600">
-                Stock On Hold: <span>{color["ON HOLD"]}</span>
-              </p>
-              <p className="text-gray-600">
-                Total Stock: <span>{color["TOTAL STOCK"]}</span>
-              </p>
-            </div>
-          ))}
-        </div>
-      </main>
-
-      <Outlet />
+          <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 w-full text-sm">
+            {colorStock.map((color) => (
+              <div
+                key={color["PRODUCT NAME"]}
+                className="p-2 bg-white border border-gray-200 text-black flex flex-col items-start rounded-md shadow hover:bg-gray-50 transition"
+              >
+                <label className="font-semibold">{color["PRODUCT NAME"]}</label>
+                <p className="text-green-600">
+                  Current Stock: <span>{color["PRESENT STOCK"]}</span>
+                </p>
+                <p className="text-yellow-600">
+                  Stock On Hold: <span>{color["ON HOLD"]}</span>
+                </p>
+                <p className="text-gray-600">
+                  Total Stock: <span>{color["TOTAL STOCK"]}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        </main>
+      ) : (
+        <Outlet />
+      )}
     </div>
   );
 }
